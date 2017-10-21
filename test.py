99aa127358b79be00f9096280ea30364f8fb85f8
autocommit <autocommit>
2017-10-21 07:14:29 +0000

    auto 0 316 4412

--- test.py
+++ test.py
@@ -19,7 +19,7 @@ for row in result:
     print(row)
 
 print('COPY TO')
-print(c.execute("COPY derp (id,derp) TO '/tmp/derp'"))
+print(c.execute("COPY derp (id,derp) TO '/tmp/derp'").statusId)
 for buf in c.copy("COPY derp (id,derp) TO STDOUT"):
     print(repr(buf))
 
