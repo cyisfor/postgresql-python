d0a179ec6872871dea98202a97f69a0e4ab31763
autocommit <autocommit>
2017-10-21 06:40:32 +0000

    auto 0 316 4435

--- postgresql/connection.py
+++ postgresql/connection.py
@@ -167,9 +167,9 @@ class Result(list):
 		self.fields = []
 		self.types = []
 		for c in range(interface.nfields(raw)):
+			print("ummmm",self.statusId)
 			self.fields.append(self.decode(ctypes.string_at(interface.fname(raw,c))))
 			self.types.append(int(interface.ftype(raw,c)))
-		print("ummmm",self.statusId)
 		for r in range(interface.ntuples(raw)):
 			row = list()
 			for c in range(interface.nfields(raw)):
