4500dfa16fac0e16e227dd3666c9300e04016142
autocommit <autocommit>
2017-10-21 06:35:44 +0000

    auto 0 319 4450

--- postgresql/interface.py
+++ postgresql/interface.py
@@ -62,8 +62,8 @@ class send:
 #executeOnce = lib.PQexecParams
 #prepare = lib.PQprepare
 class result(c_void_p):
-	def __str__(self):
-		return super().__str__()
+	def __repr__(self):
+		return super().__repr__()
 	def __getattr__(self,n):
 		print("ummmm",n)
 		return super().__getattr__(n)
