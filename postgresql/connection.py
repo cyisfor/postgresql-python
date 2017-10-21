488558b07e27cf9ef1e25af86e0ea5af68c854fa
autocommit <autocommit>
2017-10-21 07:06:26 +0000

    auto 0 317 4447

--- postgresql/connection.py
+++ postgresql/connection.py
@@ -530,6 +530,7 @@ class Connection:
 				continue
 			elif code == -1:
 				print("copy DONE")
+				yield oneresult(self.results(raw,stmt))
 				return
 			elif code == -2:
 				raise SQLError(stmt,getError(raw))
