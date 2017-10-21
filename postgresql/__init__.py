74494c5177ab22dd41ecbac59e2e582111db33e2
autocommit <autocommit>
2017-10-21 05:57:08 +0000

    auto 0 217 3604

--- postgresql/__init__.py
+++ postgresql/__init__.py
@@ -1,12 +1,14 @@
 import sys
+mod = sys.modules[__name__]
 class stagetwo:
 	__path__ = __path__
 	__spec__ = __spec__
 	def __call__(self):
 		print("snorgling")
-		import init
+		from . import init
 		import sys
 		sys.modules[__name__] = init
 	def __getattr__(self,n):
 		print("ey?",n)
+		return getattr(mod,n)
 sys.modules[__name__] = stagetwo()
